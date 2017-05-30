2267a9a9c96909ac09e49f1c99ed259b05c3c233
Sean Brage <sean@layer3tv.com>
2017-05-30 14:06:08 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace time_greeter_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            int hour = currentTime.Hour;
+
+            string Greeting(int h)
+            {
+                string greeting = "";
+                if (h < 12)
+                {
+                    greeting = "Good Morning!";
+                }
+
+                if (h >= 12 && h < 17)
+                {
+                    greeting = "Good Afternoon!";
+                }
+
+                if (h >= 17 && h < 21)
+                {
+                    greeting = "Good Evening!";
+                }
+
+                if (h >= 21)
+                {
+                    greeting = "Good Night! Seriously, go to bed.";
+                }
+
+                return greeting;
+            }
+
+            Console.WriteLine(Greeting(hour));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Time-Greeter C#
+
+A basic console application demonstrating the use of logical operators in C#.
